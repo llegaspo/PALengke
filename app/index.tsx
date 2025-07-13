8236d2818cae228ed936db4a4f5854088d7ab319
mijimmi <jrmijares@up.edu.ph>
2025-07-13 16:05:10 +0800

    fixes

--- app/index.tsx
+++ app/index.tsx
@@ -1,5 +1,5 @@
 import { StyleSheet, Text, View } from 'react-native';
-import { db } from "../firebase/firebaseconfig"; 
+import { db } from "../firebase/firebaseConfig"; 
 
 const Home = () => {
   return (
