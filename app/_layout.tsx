d9b66c0b69d074ec7f17e835a24c79dc453566dd
llegaspo <jed.lordy123@gmail.com>
2025-07-14 17:59:16 +0800

    fixed shop

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -13,88 +13,88 @@ export default function RootLayout(){
         },
         headerLeft: () => null, // Remove back button
       }}>
-      <Stack.Screen 
-        name='index' 
+      <Stack.Screen
+        name='index'
         options={{
           title: 'palengke',
           headerShown: true,
           headerLeft: () => null
         }}
       />
-      <Stack.Screen 
-        name='main/index' 
+      <Stack.Screen
+        name='main/index'
         options={{
           title: 'palengke',
           headerShown: true,
           headerLeft: () => null
         }}
       />
-      <Stack.Screen 
-        name='language/index' 
+      <Stack.Screen
+        name='language/index'
         options={{
           title: 'palengke',
           headerShown: true,
           headerLeft: () => null
         }}
       />
-      <Stack.Screen 
-        name='onboarding/index' 
+      <Stack.Screen
+        name='onboarding/index'
         options={{
           title: 'palengke',
           headerShown: true,
           headerLeft: () => null
         }}
       />
-      <Stack.Screen 
-        name='existing-storeoptions/index' 
+      <Stack.Screen
+        name='existing-storeoptions/index'
         options={{
           title: 'palengke',
           headerShown: true,
           headerLeft: () => null
         }}
       />
-      <Stack.Screen 
-        name='existing-inventory/index' 
+      <Stack.Screen
+        name='existing-inventory/index'
         options={{
           title: 'palengke',
           headerShown: true,
           headerLeft: () => null
         }}
       />
-      <Stack.Screen 
-        name='home/index' 
+      <Stack.Screen
+        name='home/index'
         options={{
           title: 'palengke',
           headerShown: true,
           headerLeft: () => null
         }}
       />
-      <Stack.Screen 
-        name='shop/index' 
+      <Stack.Screen
+        name='shop/index'
         options={{
           title: 'palengke',
           headerShown: true,
           headerLeft: () => null
         }}
       />
-      <Stack.Screen 
-        name='chat/index' 
+      <Stack.Screen
+        name='chat/index'
         options={{
           title: 'palengke',
           headerShown: true,
           headerLeft: () => null
         }}
       />
-      <Stack.Screen 
-        name='wallet/index' 
+      <Stack.Screen
+        name='wallet/index'
         options={{
           title: 'palengke',
           headerShown: true,
           headerLeft: () => null
         }}
       />
-      <Stack.Screen 
-        name='share/index' 
+      <Stack.Screen
+        name='share/index'
         options={{
           title: 'palengke',
           headerShown: true,
